5000f724c724cefa7883fa5a285a7e5b35591286
thanhanphan17 <thanhanphan17@gmail.com>
2023-12-08 02:06:24 +0700

    run spring boot with socket component

--- src/main/java/chatapp/Main.java
+++ src/main/java/chatapp/Main.java
@@ -1,13 +1,25 @@
 package chatapp;
 
+import chatapp.internal.logger.AppLogger;
+import chatapp.socket.ServerSocketComponent;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Main {
+import java.util.logging.Logger;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
-	}
+@SpringBootApplication
+public class Main implements CommandLineRunner {
+    private static final Logger logger = AppLogger.getLogger();
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        ServerSocketComponent server = new ServerSocketComponent();
+        server.run();
+    }
 
 }
