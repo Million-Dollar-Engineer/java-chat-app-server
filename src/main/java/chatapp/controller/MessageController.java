35bebe76723f001d4fb9f5077cd230137c7ff631
thanhanphan17 <thanhanphan17@gmail.com>
2023-12-09 04:13:01 +0700

    add message service and message controller

--- src/main/java/chatapp/controller/MessageController.java
+++ src/main/java/chatapp/controller/MessageController.java
@@ -1,4 +1,32 @@
 package chatapp.controller;
 
+import chatapp.entity.MessageEntity;
+import chatapp.entity.PersonalMessageEntity;
+import chatapp.internal.result.Result;
+import chatapp.repository.IMessageRepository;
+import chatapp.repository.impl.MessageRepository;
+import chatapp.service.MessageService;
+
 public class MessageController {
+    private final MessageService service;
+
+    public MessageController() {
+        IMessageRepository repository = new MessageRepository();
+        this.service = new MessageService(repository);
+    }
+
+    public static void main(String[] args) {
+        MessageController controller = new MessageController();
+        controller.testSaveMessage();
+    }
+
+    void testSaveMessage() {
+        MessageEntity messageEntity = new PersonalMessageEntity("1", "2", "Hello");
+        Result<MessageEntity> result = service.save(messageEntity);
+        if (result.getError() != null) {
+            System.out.println("Error occurred: " + result.getError().getMessage());
+        } else {
+            System.out.println("Message saved: " + result.getObject().getMessage());
+        }
+    }
 }
--- src/main/java/chatapp/service/MessageService.java
+++ src/main/java/chatapp/service/MessageService.java
@@ -1,4 +1,37 @@
 package chatapp.service;
 
+import chatapp.entity.MessageEntity;
+import chatapp.internal.result.Result;
+import chatapp.repository.IMessageRepository;
+
 public class MessageService {
+    private final IMessageRepository repo;
+
+    public MessageService(IMessageRepository repo) {
+        this.repo = repo;
+    }
+
+    public Result<MessageEntity> save(MessageEntity messageEntity) {
+        if (messageEntity == null) {
+            return Result.failure(new IllegalArgumentException("Message entity cannot be null"));
+        }
+
+        if (isNullOrEmpty(messageEntity.getSenderId())) {
+            return Result.failure(new IllegalArgumentException("Message sender id cannot be null or empty"));
+        }
+
+        if (isNullOrEmpty(messageEntity.getRecipientId())) {
+            return Result.failure(new IllegalArgumentException("Message recipient id cannot be null or empty"));
+        }
+
+        if (isNullOrEmpty(messageEntity.getMessage())) {
+            return Result.failure(new IllegalArgumentException("Message content cannot be null or empty"));
+        }
+
+        return repo.save(messageEntity);
+    }
+
+    private boolean isNullOrEmpty(String str) {
+        return str == null || str.trim().isEmpty();
+    }
 }
