783bfc908bb181dfab5be76d806c8fa92c40a388
thanhanphan17 <thanhanphan17@gmail.com>
2023-12-09 04:12:42 +0700

    add save method message repository

--- src/main/java/chatapp/repository/IMessageRepository.java
+++ src/main/java/chatapp/repository/IMessageRepository.java
@@ -1,4 +1,13 @@
 package chatapp.repository;
 
+import chatapp.entity.MessageEntity;
+import chatapp.internal.result.Result;
+
+import java.util.List;
+
 public interface IMessageRepository {
+
+    Result<MessageEntity> save(MessageEntity messageEntity);
+
+    Result<List<MessageEntity>> findMessagesByReceiverId(String receiverId);
 }
--- src/main/java/chatapp/repository/impl/MessageRepository.java
+++ src/main/java/chatapp/repository/impl/MessageRepository.java
@@ -1,4 +1,43 @@
 package chatapp.repository.impl;
 
-public class MessageRepository {
+import chatapp.dao.GroupMessageDAO;
+import chatapp.dao.MessageDAO;
+import chatapp.dao.PersonalMessageDAO;
+import chatapp.entity.MessageEntity;
+import chatapp.entity.PersonalMessageEntity;
+import chatapp.internal.database.Postgres;
+import chatapp.internal.result.Result;
+import chatapp.repository.IMessageRepository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MessageRepository implements IMessageRepository {
+    private final Map<Class<? extends MessageEntity>, MessageDAO> daoMap = new HashMap<>();
+
+    public MessageRepository() {
+        Postgres database = Postgres.getInstance();
+        this.daoMap.put(PersonalMessageEntity.class, new PersonalMessageDAO(database.getConnection()));
+        this.daoMap.put(MessageEntity.class, new GroupMessageDAO(database.getConnection()));
+    }
+
+    @Override
+    public Result<MessageEntity> save(MessageEntity messageEntity) {
+        MessageDAO messageDAO = createMessageDAO(messageEntity);
+        return messageDAO.save(messageEntity);
+    }
+
+    @Override
+    public Result<List<MessageEntity>> findMessagesByReceiverId(String receiverId) {
+        return null;
+    }
+
+    private MessageDAO createMessageDAO(MessageEntity messageEntity) {
+        MessageDAO messageDAO = daoMap.get(messageEntity.getClass());
+        if (messageDAO == null) {
+            throw new IllegalArgumentException("Unsupported message entity type: " + messageEntity.getClass().getName());
+        }
+        return messageDAO;
+    }
 }
